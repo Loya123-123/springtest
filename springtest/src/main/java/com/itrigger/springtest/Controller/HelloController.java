c4c0692a55b5916773509a9961d4850fc9063e9e
Loya123-123 <54530839+Loya123-123@users.noreply.github.com>
2020-01-14 14:40:33 +0800

    接口部分对象信息传输需要resource

--- springtest/src/main/java/com/itrigger/springtest/Controller/HelloController.java
+++ springtest/src/main/java/com/itrigger/springtest/Controller/HelloController.java
@@ -1,16 +1,21 @@
 package com.itrigger.springtest.Controller;
 
 import com.itrigger.springtest.Bean.person;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import javax.annotation.Resource;
 
 @Controller
 public class HelloController {
+    @Resource
     person person;
     @ResponseBody
     @RequestMapping("/hello")
     public String hello() {
-        return person.toString();
+        String men=person.toString();
+        System.out.println(men);
+        return men;
     }
 }
